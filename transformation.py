271c9ce7c21a5a4214063a91d5089225f304cafb
hudson_silverio <hudsonpksj@gmail.com>
2025-02-21 11:03:14 -0300

    retirando a uniao de colunas, pronto para entrega

--- transformation.py
+++ transformation.py
@@ -17,27 +17,6 @@ def limpar_dados(dados):
     # Aplicar a função a todas as células do DataFrame
     dados_limpos = dados.applymap(remover_emails)
 
-    # Unir colunas específicas e criar novas colunas
-    if '(UTC)3' in dados_limpos.columns and 'Minutes' in dados_limpos.columns:
-        dados_limpos['(UTC)3_Minutes'] = (
-            dados_limpos['(UTC)3'].astype(str)
-            + ' '
-            + dados_limpos['Minutes'].astype(str)
-        )
-
-    if 'Spent' in dados_limpos.columns and 'Position' in dados_limpos.columns:
-        dados_limpos['Spent_Position'] = (
-            dados_limpos['Spent'].astype(str)
-            + ' '
-            + dados_limpos['Position'].astype(str)
-        )
-
-    # Remover as colunas originais após a união
-    dados_limpos = dados_limpos.drop(
-        columns=['(UTC)3', 'Minutes', 'Spent', 'Position'], 
-        errors='ignore'
-    )
-
     # Retornar tanto os dados limpos quanto a ordem original
     return dados_limpos, colunas_originais
 
